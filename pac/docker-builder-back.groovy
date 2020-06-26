130c00f882f9f71f093acf2de63f3b6dde5cddc4
d-rom <daria.ruiz-ogarriomarassanova@telefonica.com>
2020-06-26 18:34:56 +0200

    Updated versions

--- pac/docker-builder-back.groovy
+++ pac/docker-builder-back.groovy
@@ -13,7 +13,7 @@ pipeline {
         GIT_REPONAME = 'vision_valley_demo'
         AWS_DEFAULT_REGION = 'eu-west-3'
         AWS_DEFAULT_OUTPUT = 'json'
-        CV_VER = '1.8'
+        CV_VER = '1.9'
     }
     stages {
         stage ('build computer vision backend') {
