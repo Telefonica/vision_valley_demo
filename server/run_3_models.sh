23d01bc895a4ceef9d392fe5ca70279b93f6d180
d-rom <daria.ruiz-ogarriomarassanova@telefonica.com>
2020-06-30 16:39:56 +0200

    Functional docker compose structure

--- server/run_3_models.sh
+++ server/run_3_models.sh
@@ -3,7 +3,7 @@ source /opt/intel/openvino/bin/setupvars.sh
 
 
 sleep 10
-python3 main.py -i rtsp://127.0.0.1:8554/video_count -o rtmp://127.0.0.1:1935/show/stream -f 25 -m /home/person-detection-retail-0013.xml -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 & \
+python3 python3 main.py -i rtsp://127.0.0.1:8554/video_count -o rtmp://localhost/show/stream -f 25 -m /opt/intel/openvino/deployment_tools/tools/model_downloader/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt/person-detection-retail-0013.xml -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 & \
 python3 tensorflow_infer.py -i rtsp://127.0.0.1:8556/video_mask -o rtmp://127.0.0.1:1935/maskshow/stream -f 30 & \
 python3 social_distancing_analyser.py -i rtsp://127.0.0.1:8555/video -m /home/person-detection-retail-0013.xml -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 -o rtmp://127.0.0.1:1935/distshow/stream -f 25 &&
 
